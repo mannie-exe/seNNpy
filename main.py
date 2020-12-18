288993f7745cbd93c28daae603ea81376cfbba78
mannie.exe <root@unknown.energy>
2020-12-18 15:48:57 -0800

    Add a few comments and dividers to the main file

--- main.py
+++ main.py
@@ -4,97 +4,118 @@ import numpy as np
 import os
 import time
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
+
+
+# Load and vectorize dataset
+# -----------------------------------------------
 path_to_file = './training_data/messages.txt'
-
 text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-
 vocab = sorted(set(text))
 
-char2idx = {u:i for i, u in enumerate(vocab)}
+char2idx = {u: i for i, u in enumerate(vocab)}
 idx2char = np.array(vocab)
-
 text_as_int = np.array([char2idx[c] for c in text])
+# -----------------------------------------------
 
-seq_length = 100
-examples_per_epoch = len(text)
 
-char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
-
-sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
-
+# Batch and create TensorFlow training batches
+# -----------------------------------------------
 def split_input_target(chunk):
     input_text = chunk[:-1]
     target_text = chunk[1:]
     return input_text, target_text
 
+
+seq_length = 100
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
 dataset = sequences.map(split_input_target)
 
-BATCH_SIZE = 64
-
-BUFFER_SIZE = 1000
+BATCH_SIZE = 128
+BUFFER_SIZE = 4096
 
 dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+# -----------------------------------------------
 
-vocab_size = len(vocab)
 
-embedding_dim = 256
-
-rnn_units = 1024
-
+# Build model and loss function
+# -----------------------------------------------
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
-        tf.keras.layers.GRU(rnn_units, return_sequences=True, stateful=True, recurrent_initializer='glorot_uniform'),
+        tf.keras.layers.Embedding(
+            vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
+        tf.keras.layers.GRU(rnn_units, return_sequences=True,
+                            stateful=True, recurrent_initializer='glorot_uniform'),
         tf.keras.layers.Dense(vocab_size)
     ])
     return model
 
-model = build_model(vocab_size=vocab_size, embedding_dim=embedding_dim, rnn_units=rnn_units, batch_size=BATCH_SIZE)
-
-for input_example_batch, taarget_example_batch in dataset.take(1):
-    example_batch_predictions = model(input_example_batch)
-    print(example_batch_predictions.shape, "# (batch_size, sequence_lenght, vocab_size)")
 
 def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
+
+vocab_size = len(vocab)
+embedding_dim = 256
+rnn_units = 1024
+
+model = build_model(vocab_size=vocab_size, embedding_dim=embedding_dim,
+                    rnn_units=rnn_units, batch_size=BATCH_SIZE)
+
 model.compile(optimizer='adam', loss=loss)
+# -----------------------------------------------
+
+
+# Train model
+# -----------------------------------------------
+EPOCHS = 10
 
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt_{epoch}')
 
-checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True)
-
-EPOCHS = 30
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix, save_weights_only=True)
 
 history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+# -----------------------------------------------
 
-model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
-model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
-model.build(tf.TensorShape([1, None]))
 
+# Re-build model to generate new text
+# -----------------------------------------------
 def generate_text(model, start_string):
     num_generate = 1000
+    temperature = 1.2
 
     input_eval = [char2idx[s] for s in start_string]
     input_eval = tf.expand_dims(input_eval, 0)
 
-    text_generated = []
-
-    temperature = 1.2
-
     model.reset_states()
+
+    text_generated = []
     for i in range(num_generate):
         predictions = model(input_eval)
         predictions = tf.squeeze(predictions, 0)
-
         predictions = predictions / temperature
-        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
-
+        predicted_id = tf.random.categorical(
+            predictions, num_samples=1)[-1, 0].numpy()
         input_eval = tf.expand_dims([predicted_id], 0)
 
         text_generated.append(idx2char[predicted_id])
 
     return (start_string + ''.join(text_generated))
 
-print(generate_text(model, start_string=u'pog '))
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+print(generate_text(model, start_string=u'today, I '))
+# -----------------------------------------------
