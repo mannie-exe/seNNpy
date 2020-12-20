2e18769c9108f652f47f6f49be4e24d192ff273b
mannie.exe <root@unknown.energy>
2020-12-20 13:50:13 -0800

    Fix URL removal from message clean-up script

--- training_data/clean_data.py
+++ training_data/clean_data.py
@@ -5,28 +5,38 @@ import re
 import json
 
 
-raw_messages_path = os.path.abspath('export.json')
+URL_PATTERN = re.compile(
+    "((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)", flags=re.I
+)
+
+raw_messages_path = os.path.abspath("export.json")
 raw_messages_exist = os.path.isfile(raw_messages_path)
 if not raw_messages_exist:
-    sys.exit("Use DiscordChatExporter and export a JSON format message list as 'export.json', and paste the file here")
+    sys.exit(
+        "Use DiscordChatExporter to export a JSON format message list as 'export.json', then paste the file here"
+    )
+
 
 def clean_messages(accum, message):
-    URL_PATTERN = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', flags=re.I)
-    if not message['author']['isBot']:
-        if re.match(URL_PATTERN, message['content']):
-            message['content'] = re.sub(URL_PATTERN, '', message['content'])
-        if message['content'].strip():
-            accum.append(message['content'])
+    if not message["author"]["isBot"]:
+        message["content"] = re.sub(URL_PATTERN, "", message["content"]).strip()
+        if message["content"]:
+            accum.append(message["content"])
     return accum
 
 
-with open(raw_messages_path, encoding='utf-8') as raw_messages:
-    raw_messages = json.loads(raw_messages.read())['messages']
+with open(raw_messages_path, encoding="utf-8") as raw_messages:
+    raw_messages = json.loads(raw_messages.read())["messages"]
     clean_messages = reduce(clean_messages, raw_messages, [])
 
-    new_file_path = os.path.abspath('messages.txt')
-    with open(new_file_path, 'w', encoding='utf-8') as new_messages_file:
-        new_messages_file.write('\n'.join(clean_messages))
+    new_file_path = os.path.abspath("messages.txt")
+    if os.path.isfile(new_file_path):
+        os.remove(new_file_path)
+    with open(new_file_path, "w", encoding="utf-8") as new_messages_file:
+        new_messages_file.write("\n".join(clean_messages))
 
-    print('%d -> %d' % (len(raw_messages), len(clean_messages)))
-
+    print(
+        "Cleaned '{}' messages down to '{}' messages".format(
+            len(raw_messages), len(clean_messages)
+        )
+    )
