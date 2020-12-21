2a885ee1efe22e337f2ce812002e0d3820866d0b
mannie.exe <root@unknown.energy>
2020-12-20 16:13:26 -0800

    Implement new data ingestion script and add more channels

---
+++ training_data/get.bat
@@ -0,0 +1,4 @@
+.\DiscordChatExporter.CLI\DiscordChatExporter.Cli.exe export -c 328303674352730113 -f Json -o export_badlands.json -b -t Nzg4MjA3MTI5NDE2NTY0Nzc3.X9gJFw.kdV_GnlZkHDQQztANT49sK0Xzqo
+.\DiscordChatExporter.CLI\DiscordChatExporter.Cli.exe export -c 374969230770044949 -f Json -o export_dokidokiliterature.json -b -t Nzg4MjA3MTI5NDE2NTY0Nzc3.X9gJFw.kdV_GnlZkHDQQztANT49sK0Xzqo
+.\DiscordChatExporter.CLI\DiscordChatExporter.Cli.exe export -c 403600682801823744 -f Json -o export_hellotavern.json -b -t Nzg4MjA3MTI5NDE2NTY0Nzc3.X9gJFw.kdV_GnlZkHDQQztANT49sK0Xzqo
+.\DiscordChatExporter.CLI\DiscordChatExporter.Cli.exe export -c 788399540574879795 -f Json -o export_ediea2ndera.json -b -t Nzg4MjA3MTI5NDE2NTY0Nzc3.X9gJFw.kdV_GnlZkHDQQztANT49sK0Xzqo
